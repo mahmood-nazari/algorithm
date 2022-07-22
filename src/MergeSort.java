79bc557dd65d3d31db17616b2efdcb11087c432f
mahmood <mahmood-nazari>
2022-07-22 19:24:15 +0430

    MergeSort added

---
+++ src/MergeSort.java
@@ -0,0 +1,10 @@
+public class MergeSort {
+    public static void main(String[] args) {
+        int arr[] = {90,23,101,45,65,23,67,89,34,23};
+        mergeSort(arr);
+    }
+
+    private static void mergeSort(int[] arr) {
+
+    }
+}
